4fba29c77ae499429a594fb4df3c05dc44e318ee
emacec <emacec@byu.edu>
2025-03-20 11:02:12 -0600

    added sort

--- backend/Mission11/Mission11/Controllers/BookController.cs
+++ backend/Mission11/Mission11/Controllers/BookController.cs
@@ -16,10 +16,22 @@ namespace Mission11.Controllers
 
 
         [HttpGet("AllBooks")]
-        public IEnumerable<Book> GetBooks()
+        public IActionResult GetBooks(int pageHowMany = 5, int pageNum = 1)
         {
-            var something = _bookContext.Books.ToList();
-            return something;
+            var something = _bookContext.Books
+                .Skip((pageNum-1)* pageHowMany)
+                .Take(pageHowMany)
+                .ToList();
+
+            var totalNumProjects = _bookContext.Books.Count();
+
+            var someObject = new
+            {
+                Books = something,
+                TotalNumBooks = totalNumProjects
+            };
+
+            return Ok(someObject);
         }
 
         [HttpGet("FunctionalBooks")]
