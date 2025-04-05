3d4bed61426a06c1b2bbbb9b81e44d4f2eb9bd88
emacec <emacec@byu.edu>
2025-04-04 22:15:08 -0600

    book types

--- frontend/src/components/CategoryFilter.tsx
+++ frontend/src/components/CategoryFilter.tsx
@@ -15,7 +15,7 @@ function CategoryFilter({
     const fetchCategories = async () => {
       try {
         const response = await fetch(
-          'https://localhost:5000/Book/GetBookTypes'
+          'https://bookproject-camberos-backend-dee4bqbqgfdzdvhf.eastus-01.azurewebsites.net/book/GetBookTypes'
         );
         const data = await response.json();
         console.log('Fetched categories:', data);
